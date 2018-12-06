4645434d837c7acc758c164b087615d9c56d00cf
cdc-jtmiller <miller.jeremyt@gmail.com>
2018-12-06 12:12:23 -0500

    Adding project to Github

---
+++ ReadCSV/ReadCSV.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace ReadCSV
+{
+    partial class ReadCSV
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnChoose = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(93, 27);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(583, 309);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnChoose
+            // 
+            this.btnChoose.Location = new System.Drawing.Point(12, 27);
+            this.btnChoose.Name = "btnChoose";
+            this.btnChoose.Size = new System.Drawing.Size(75, 23);
+            this.btnChoose.TabIndex = 1;
+            this.btnChoose.Text = "Choose File";
+            this.btnChoose.UseVisualStyleBackColor = true;
+            this.btnChoose.Click += new System.EventHandler(this.btnChoose_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // ReadCSV
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(688, 348);
+            this.Controls.Add(this.btnChoose);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "ReadCSV";
+            this.Text = "ReadCSV";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnChoose;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ ReadCSV/ReadCSV.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using NReco.Csv;
+
+namespace ReadCSV
+{
+    public partial class ReadCSV : Form
+    {
+        public ReadCSV()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChoose_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string sFileName = "";
+                dataGridView1.DataSource = null;
+                dataGridView1.Rows.Clear();
+                dataGridView1.Columns.Clear();
+                dataGridView1.Refresh();
+                OpenFileDialog sDialogFName = new OpenFileDialog();
+                sDialogFName.Title = "Open CSV file";
+                sDialogFName.Filter = "CSV Files (*.csv)|*.csv";
+                if (sDialogFName.ShowDialog() == DialogResult.OK)
+                {
+                    sFileName = sDialogFName.FileName;
+                    ImportCsvFile(sFileName);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error loading data : " + ex.Message);
+            }
+        }
+
+
+        public void ImportCsvFile(string filename)
+        {
+            using (StreamReader sr= new StreamReader(filename))
+            {
+                DataTable netData = new DataTable();
+                string[] headers = sr.ReadLine().Split(',');
+                var csvReader = new CsvReader(sr, ",");
+                for (int i = 0; i < headers.Count(); i++)
+                {
+                    netData.Columns.Add(headers[i], typeof(string));
+                }
+
+                {
+                    while (csvReader.Read())
+                    {
+                        DataRow dr = netData.NewRow();
+                        for (int i = 0; i < csvReader.FieldsCount; i++)
+                        {
+                            dr[i] = csvReader[i];
+                        }
+                        netData.Rows.Add(dr);
+                    }
+                    dataGridView1.DataSource = netData;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
